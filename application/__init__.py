596fcc16408bf3881b1af927c37c7dbbffa5e8af
iamkishoremahto <iamkishoremahto@gmail.com>
2023-05-11 12:15:12 +0530

    create project structure

---
+++ application/__init__.py
@@ -0,0 +1,10 @@
+from flask import  Flask
+import os
+app = Flask(__name__)
+
+cwd = os.getcwd()
+
+temp_upload_folder = os.path.join(cwd,'application/static/temp_uploaded_image')
+app.config['UPLOAD_FOLDER'] = temp_upload_folder
+
+from application import  routes
