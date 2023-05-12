402fd3657f535d24e91cab92f60ee04c8092a15b
iamkishoremahto <iamkishoremahto@gmail.com>
2023-05-12 16:19:55 +0530

    implement remove background

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,38 @@
-from flask import render_template
+import json
 
+from flask import render_template,request,url_for
+from rembg import remove
+from PIL import Image
+import os
 from application import app
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# @app.route('/two')
+# def home2():
+#     return render_template('home.html')
+@app.route('/upload',methods = ['POST'])
+def upload():
+    try:
+        os.remove('application/static/temp_uploaded_image/output.png')
+    except:
+        pass
+    file = request.files['file']
+    file_path = f'application/static/temp_uploaded_image/{file.filename}'
+    file.save(file_path)
+    input_file = Image.open(file_path)
+    output = remove(input_file)
+    output.save(f'application/static/temp_uploaded_image/output.png')
+    os.remove((file_path))
+    image_url = url_for('static', filename = 'temp_uploaded_image/output.png')
+    with app.test_request_context():
+        url = url_for('static', filename='temp_uploaded_image/output.png')
+        # return  ("{{ url_for('static',filename='temp_uploaded_image/output.png') }}")
+    # return "application/static/temp_uploaded_image/output.png"
+    return url
+
+
+
---
+++ application/static/js/main.js
@@ -0,0 +1,42 @@
+
+upload_btn.addEventListener("click",function(){
+    document.getElementById("file_upload").click()
+    
+  });
+
+file_upload.addEventListener("change", function(){
+    fileInput = document.getElementById('file_upload')
+    file = fileInput.files[0]
+    upload(file)
+    // console.log(file.name)
+})
+function change_upload_area(image_url){
+    document.getElementById('home_wrapper').innerHTML = `
+    
+    
+     
+    <div class="col-md-8">
+    <div id="show_area">
+        <img src = "${image_url}" alt='removebg_image'
+            class="img-fluid">
+        <a href = ${image_url} class='btn btn-primary'
+            download>Download</a>
+    </div>
+</div>
+    `
+}
+async function upload(file){
+    var formData = new FormData();
+    formData.append("file", file);
+    var request = new XMLHttpRequest();
+    request.open("POST", "/upload");
+    await request.send(formData);
+    request.onload = function(){
+        var image_url = request.responseText;
+        change_upload_area(image_url)
+        console.log(image_url)
+    }
+    
+
+}
+ 
